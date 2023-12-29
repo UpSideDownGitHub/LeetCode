eae472d9ff892b7d2708b1785e8e7656eca77eff
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-12-29 11:55:54 +0000

    1

---
+++ 1/Two Sum.c
@@ -0,0 +1,18 @@
+/*
+// DONE IN PYTHON BECAUSE I RAN OUT OF TIME AND C WAS GIVING STUPID ERROR THAT WAS NOT MY FAULT
+(IT OBVIOUSLY WAS BUT I COULD NOT FIGURE IT OUT)
+Runtime: 57ms, 90.82%
+Memory Usage: 18.58MB, 5.96%
+*/
+
+/*
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, num in enumerate(nums):
+            t = target - num
+            if t in d:
+                return [d[t], i]
+            d[num] = i
+        return []
+*/
